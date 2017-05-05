d20e123165446da7cee69e9cbe81a5f20bdf2065
wiserkevin <kyu@YuKevin.local>
2017-05-05 15:53:55 -0700

    made sphere move
    using keys to control the sphere

---
+++ Assets/bob.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class bob : MonoBehaviour {
+
+	public float Speed;
+	private Rigidbody rb;
+	// Use this for initialization
+	void Start () {
+		rb = GetComponent<Rigidbody> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		// Gets input
+		float hSpeed = Input.GetAxis("Horizontal") * Speed;
+		float vSpeed = Input.GetAxis ("Vertical") * Speed;
+
+		// Pushes rb
+		rb.AddForce (new Vector3 (hSpeed, 0, vSpeed));
+	}
+}
