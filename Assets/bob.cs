38b6e6d38b4b676297a39d5c4d1c3616c4104914
alvin1004da <alee@LeeAlvin.local>
2017-05-05 16:11:42 -0700

    Edit Jump
    Can jump when you press spacebar

--- Assets/bob.cs
+++ Assets/bob.cs
@@ -14,10 +14,15 @@ public class bob : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 		// Gets input
-		float hSpeed = Input.GetAxis("Horizontal") * Speed;
+		float hSpeed = Input.GetAxis ("Horizontal") * Speed;
 		float vSpeed = Input.GetAxis ("Vertical") * Speed;
+		float jSpeed = 0;
+
+		// Only allows jump on floor
+		if (rb.position.y <= 0.5) 
+			jSpeed = Input.GetAxis ("Jump") * Speed * 100;
 
 		// Pushes rb
-		rb.AddForce (new Vector3 (hSpeed, 0, vSpeed));
+		rb.AddForce (new Vector3 (hSpeed, jSpeed, vSpeed));
 	}
 }
